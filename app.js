6112ed36ca2a7c43411033d763448b0b81a5fca7
Jamie Turner <jamiehattie@gmail.com>
2020-04-26 17:18:34 +0100

    add files

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const http = require('http');
 const port = process.env.PORT || 3000
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+var url = require('url');
+var fs = require('fs');
+var uc = require('upper-case'); 
+const server = http.createServer((req, res) => {  
+  fs.readFile('demofile1.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+	res.write(uc.upperCase("Hello World!"));
+    return res.end();
+  });
 });
 
 server.listen(port,() => {
